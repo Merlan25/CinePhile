5fb0294777b6443308e1d87f21ebe2a58f68d894
Merlan <merlan@gmail.com>
2023-12-03 16:34:41 +0500

    first commit

---
+++ assets/SerialId-c8f5e420.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Single-3b2725ad.js";import{o as a,c as t,a as o}from"./index-66d46d8f.js";const s={class:"main"},i={__name:"SerialId",setup(c){return(r,_)=>(a(),t("main",s,[o(e,{type:"tv"})]))}};export{i as default};
---
+++ assets/Serials-deef58a0.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ListContent-fa6bdc30.js";import{o as a,c as t,a as s}from"./index-66d46d8f.js";const o={class:"main"},i={__name:"Serials",setup(c){return(r,_)=>(a(),t("main",o,[s(e,{type:"tv"})]))}};export{i as default};
